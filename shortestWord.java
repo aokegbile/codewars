0a3e2c9e8e7ed53f8627ffea4c13b4ec70faaa10
Okegbile, Akin <akin.okegbile.contractor@bbva.com>
2018-04-05 12:00:03 -0500

    2nd commit

---
+++ shortestWord.java
@@ -0,0 +1,12 @@
+public class Kata {
+    public static int findShort(String s) {
+        int shortest=0;
+        String[] arr = s.split(" ");
+        for(int i = 0; i<arr.length;i++){
+            int strLength = arr[i].length();
+            if (shortest==0){ shortest = strLength;}
+            else if(strLength<shortest){ shortest = strLength;}
+        }
+        return shortest;
+    }
+}
